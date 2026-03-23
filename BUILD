62c884339924a51953b849d97aea5b9508a17be1
Jacek Centkowski <geminica.programs@gmail.com>
2026-03-23 23:16:26 +0100

    Add GET endpoint for reading decrypted API tokens
    Self-only auth: returns 403 even for server admins accessing
    another user's token. This is an intentional asymmetry with PUT,
    which allows admin access, because reading plaintext secrets
    should be restricted to the token owner.
    
    Returns the decrypted token as JSON or 404 if no token is stored
    for the requesting user's provider subsection.
    
    Bug: Issue 493637568
    Change-Id: I8b9f04c01988214cfe89e5c84ed9f70864c0a080

--- BUILD
+++ BUILD
@@ -30,8 +30,8 @@ java_library(
 )
 
 gerrit_plugin_tests(
-    name = "ai-review-agent-provider_tests",
-    srcs = glob(["src/test/java/**/*.java"]),
+    name = src.split("/")[-1].replace(".java", ""),
+    srcs = [src],
     tags = ["ai-review-agent-provider"],
     deps = [
         ":ai-review-agent-provider__plugin",
@@ -39,7 +39,7 @@ gerrit_plugin_tests(
         ":test-utils",
         "//plugins/secure-config",
     ],
-)
+) for src in glob(["src/test/java/**/*IT.java"])]
 
 java_library(
     name = "secure-config-neverlink",
---
+++ src/test/java/com/gerritforge/gerrit/plugins/ai/provider/GetTokenIT.java
@@ -0,0 +1,113 @@
+// Copyright (C) 2026 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.ai.provider;
+
+import static com.gerritforge.gerrit.plugins.ai.provider.AiReviewProviderModule.API_TOKEN_ENDPOINT;
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.RestResponse;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gson.Gson;
+import com.google.inject.Key;
+import com.googlesource.gerrit.plugins.secureconfig.Codec;
+import org.junit.Before;
+import org.junit.Test;
+
+@TestPlugin(
+    name = "ai-review-agent-provider",
+    sysModule = "com.gerritforge.gerrit.plugins.ai.provider.TestAiReviewProviderModule")
+public class GetTokenIT extends LightweightPluginDaemonTest {
+  private final Gson gson = new Gson();
+  private String pluginName;
+  private VersionedAiUserData.Factory tokenDataFactory;
+  private Codec codec;
+
+  @Before
+  public void setUp() {
+    pluginName = plugin.getSysInjector().getInstance(Key.get(String.class, PluginName.class));
+    tokenDataFactory = plugin.getSysInjector().getInstance(VersionedAiUserData.Factory.class);
+    codec = plugin.getSysInjector().getInstance(Codec.class);
+  }
+
+  @Test
+  public void shouldGetTokenForSelf() throws Exception {
+    String token = "my-secret-token";
+    AddToken.Input input = new AddToken.Input();
+    input.token = token;
+
+    userRestSession.put(getTokenUri("self"), input).assertCreated();
+
+    RestResponse response = userRestSession.get(getTokenUri("self"));
+    response.assertOK();
+
+    GetToken.Output out = gson.fromJson(response.getReader(), GetToken.Output.class);
+    assertThat(out.token).isEqualTo(token);
+  }
+
+  @Test
+  public void shouldReturnNotFoundWhenTokenNotSet() throws Exception {
+    userRestSession.get(getTokenUri("self")).assertNotFound();
+  }
+
+  @Test
+  public void shouldReturnForbiddenWhenGettingAnotherUserToken() throws Exception {
+    AddToken.Input input = new AddToken.Input();
+    input.token = "admin-token";
+
+    adminRestSession.put(getTokenUri("self"), input).assertCreated();
+
+    userRestSession.get(getTokenUri(admin.id().toString())).assertForbidden();
+  }
+
+  @Test
+  public void shouldReturnForbiddenWhenAdminGetsAnotherUserToken() throws Exception {
+    AddToken.Input input = new AddToken.Input();
+    input.token = "user-token";
+
+    userRestSession.put(getTokenUri("self"), input).assertCreated();
+
+    adminRestSession.get(getTokenUri(user.id().toString())).assertForbidden();
+  }
+
+  @Test
+  public void shouldNotReturnTokenFromDifferentProvider() throws Exception {
+    String otherProvider = "other-provider";
+    String otherToken = codec.encode("other-secret");
+    tokenDataFactory.create(user.id()).load().setToken(otherProvider, otherToken);
+
+    userRestSession.get(getTokenUri("self")).assertNotFound();
+  }
+
+  @Test
+  public void shouldReturnOwnProviderTokenWhenMultipleExist() throws Exception {
+    String otherProvider = "other-provider";
+    String otherToken = codec.encode("other-secret");
+    tokenDataFactory.create(user.id()).load().setToken(otherProvider, otherToken);
+
+    String myToken = "my-secret-token";
+    AddToken.Input input = new AddToken.Input();
+    input.token = myToken;
+    userRestSession.put(getTokenUri("self"), input).assertCreated();
+
+    RestResponse response = userRestSession.get(getTokenUri("self"));
+    response.assertOK();
+
+    GetToken.Output out = gson.fromJson(response.getReader(), GetToken.Output.class);
+    assertThat(out.token).isEqualTo(myToken);
+  }
+
+  private String getTokenUri(String account) {
+    return String.join("/", "/accounts", account, pluginName) + "~" + API_TOKEN_ENDPOINT;
+  }
+}
