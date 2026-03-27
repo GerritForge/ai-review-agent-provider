024f33da047f12c2b4c5c0f482e7243f2b5258df
Luca Milanesio <luca.milanesio@gmail.com>
2026-03-27 22:25:33 +0000

    Add ai-review-provider UI integration to Gerrit
    Move all the ai-review-provider user-experience and integration with
    Gerrit API from the gerrit-review-provider-gemini plugin.
    
    Change-Id: I05ce076a24fa859c74c2f8e973417b76b686dc20

---
+++ src/main/java/com/gerritforge/gerrit/plugins/ai/provider/HttpModule.java
@@ -0,0 +1,25 @@
+// Copyright (C) 2026 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.ai.provider;
+
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.extensions.webui.JavaScriptPlugin;
+import com.google.gerrit.extensions.webui.WebUiPlugin;
+import com.google.inject.servlet.ServletModule;
+
+public class HttpModule extends ServletModule {
+  @Override
+  protected void configureServlets() {
+    DynamicSet.bind(binder(), WebUiPlugin.class)
+        .toInstance(new JavaScriptPlugin("ai-review-agent-provider.js"));
+  }
+}
