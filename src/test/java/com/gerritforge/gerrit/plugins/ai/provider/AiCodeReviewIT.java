69a89cf18e4cb400c15b918a59846be332bd01da
Luca Milanesio <luca.milanesio@gmail.com>
2026-03-28 00:27:42 +0000

    Introduce a backend API for calling backend LLMs
    Introduce a new AiCodeReview REST-API for calling generic LLMs provided
    by a pluggable provider plugin.
    
    Also, remove the ProviderKey class and introduce a brand-new
    AiReviewProvider, which includes both the key name of the provider and
    the method for running AI-powered reviews.
    
    Lastly, remove the GetToken REST-API because of security concerns in
    leaking the user's secret, which should only be used for the purpose of
    calling the LLMs through Gerrit reviews.
    
    Change-Id: I6627cb43303d45025564836dde15c684b346e09e

---
+++ src/test/java/com/gerritforge/gerrit/plugins/ai/provider/AiCodeReviewIT.java
@@ -0,0 +1,97 @@
+// Copyright (C) 2026 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.ai.provider;
+
+import static com.gerritforge.gerrit.plugins.ai.provider.AiReviewProviderModule.AI_REVIEW_ENDPOINT;
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.acceptance.RestResponse;
+import com.google.gerrit.acceptance.Sandboxed;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.entities.Change;
+import org.junit.Test;
+
+@TestPlugin(
+    name = "ai-review-agent-provider",
+    sysModule = "com.gerritforge.gerrit.plugins.ai.provider.AiReviewProviderModule",
+    apiModule = "com.gerritforge.gerrit.plugins.ai.provider.api.AiReviewProviderApiModule")
+@Sandboxed
+public class AiCodeReviewIT extends AbstractTokenIT {
+
+  @Test
+  public void shouldGenerateAiCodeReviewWithUserToken() throws Exception {
+    addApiToken();
+
+    RestResponse aiReviewResult =
+        userRestSession.post(
+            getAiCodeReviewUri(createNewChange()),
+            new AiCodeReview.Input(TEST_PROVIDER_PLUGIN_NAME, FAKE_NUTS_MODEL, "Review this"));
+    aiReviewResult.assertOK();
+    AiCodeReview.Output aiReviewOutput =
+        readContentFromJson(aiReviewResult, AiCodeReview.Output.class);
+    assertThat(aiReviewOutput.text()).isEqualTo(AI_FEEDBACK_THIS_IS_A_REALLY_COOL_CODE_LGTM);
+  }
+
+  @Test
+  public void shouldReturnBadRequestForUnknownAiReviewProvider() throws Exception {
+    addApiToken();
+
+    RestResponse aiReviewResult =
+        userRestSession.post(
+            getAiCodeReviewUri(createNewChange()),
+            new AiCodeReview.Input("non-registered-ai-plugin", "", ""));
+    aiReviewResult.assertBadRequest();
+  }
+
+  @Test
+  public void shouldReturnBadRequestForUnknownAiModel() throws Exception {
+    addApiToken();
+
+    RestResponse aiReviewResult =
+        userRestSession.post(
+            getAiCodeReviewUri(createNewChange()),
+            new AiCodeReview.Input(TEST_PROVIDER_PLUGIN_NAME, "unknown-ai-model", ""));
+    aiReviewResult.assertBadRequest();
+  }
+
+  @Test
+  public void shouldReturnNotFoundWithoutUserToken() throws Exception {
+    RestResponse aiReviewResult =
+        userRestSession.post(
+            getAiCodeReviewUri(createNewChange()),
+            new AiCodeReview.Input(TEST_PROVIDER_PLUGIN_NAME, FAKE_NUTS_MODEL, ""));
+    aiReviewResult.assertNotFound();
+  }
+
+  private String getAiCodeReviewUri(Change change) {
+    return String.join(
+        "/",
+        "/changes",
+        change.getProject().get() + "~" + change.getId().get(),
+        pluginName + "~" + AI_REVIEW_ENDPOINT);
+  }
+
+  private void addApiToken() throws Exception {
+    AddToken.Input input = new AddToken.Input();
+    input.token = "my-secret-token";
+    input.plugin = TEST_PROVIDER_PLUGIN_NAME;
+
+    userRestSession.put(getAddTokenUri("self"), input).assertCreated();
+  }
+
+  private Change createNewChange() throws Exception {
+    PushOneCommit.Result changeToReview = createChange();
+    changeToReview.assertOkStatus();
+    return changeToReview.getChange().change();
+  }
+}
