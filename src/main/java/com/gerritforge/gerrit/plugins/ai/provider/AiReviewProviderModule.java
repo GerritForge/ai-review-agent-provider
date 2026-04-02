59a0cc013d73813d5aa390d02daa771c7a415911
Luca Milanesio <luca.milanesio@gmail.com>
2026-04-02 16:50:29 +0100

    Introduce API to list the available AI providers
    Allow the UI to understand the list of AI providers
    to use.
    
    The logic of the API is the following:
    - List all user's ai-user-data.config in the user's profile
    - Extract all the list of providers associated with the user's API keys
    - Filter the providers and return only the ones that have a plugin loaded
    
    By using the new API, the UI can have the ability to understand
    the list of providers to use and show them as options for the AI chat
    in Gerrit.
    
    Change-Id: I55839cd23c65fe79967ac54076ce6d77a4069c78

--- src/main/java/com/gerritforge/gerrit/plugins/ai/provider/AiReviewProviderModule.java
+++ src/main/java/com/gerritforge/gerrit/plugins/ai/provider/AiReviewProviderModule.java
@@ -12,22 +12,21 @@
 package com.gerritforge.gerrit.plugins.ai.provider;
 
 import com.google.gerrit.extensions.restapi.RestApiModule;
-import com.google.gerrit.extensions.restapi.RestView;
-import com.google.gerrit.extensions.restapi.TopLevelResource;
 import com.google.gerrit.server.account.AccountResource;
 import com.google.gerrit.server.change.ChangeResource;
-import com.google.inject.TypeLiteral;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.googlesource.gerrit.plugins.secureconfig.Codec;
 import com.googlesource.gerrit.plugins.secureconfig.PBECodec;
 
 public class AiReviewProviderModule extends RestApiModule {
   public static final String API_TOKEN_ENDPOINT = "apiToken";
+  public static final String API_PROVIDERS_ENDPOINT = "apiProviders";
   public static final String AI_REVIEW_ENDPOINT = "aiReview";
 
   @Override
   protected void configure() {
     put(AccountResource.ACCOUNT_KIND, API_TOKEN_ENDPOINT).to(AddToken.class);
+    get(AccountResource.ACCOUNT_KIND, API_PROVIDERS_ENDPOINT).to(GetAiProviders.class);
     post(ChangeResource.CHANGE_KIND, AI_REVIEW_ENDPOINT).to(AiCodeReview.class);
 
     install(new FactoryModuleBuilder().build(VersionedAiUserData.Factory.class));
