442e19e2f1553a104a201ec355ae28b201a99bcd
Luca Milanesio <luca.milanesio@gmail.com>
2026-04-02 16:50:29 +0100

    Introduce API to list the available AI providers
    Allow the UI to understand the list of AI providers
    to use.
    
    The logic of the API is the following:
    - List all user's ai-user-data.config in the user's profile
    - Extract all the list of providers associated with the user's API keys
    - Filter the providers and return only the ones that have a plugin loaded
    
    By using the new API, the UI can have the ability to understand
    the list of providers to use and show them as options for the AI chat
    in Gerrit.
    
    Change-Id: I55839cd23c65fe79967ac54076ce6d77a4069c78

---
+++ src/main/java/com/gerritforge/gerrit/plugins/ai/provider/GetAiProviders.java
@@ -0,0 +1,76 @@
+// Copyright (C) 2026 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.ai.provider;
+
+import com.gerritforge.gerrit.plugins.ai.provider.api.AiReviewProvider;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.extensions.registration.Extension;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountResource;
+import com.google.gerrit.server.permissions.GlobalPermission;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class GetAiProviders implements RestReadView<AccountResource> {
+  private final Provider<CurrentUser> currentUser;
+  private final VersionedAiUserData.Factory tokenDataFactory;
+  private final PermissionBackend permissionBackend;
+  private final DynamicSet<AiReviewProvider> aiReviewProviders;
+
+  public record ProviderInfo(String plugin, String displayName, Set<String> models) {}
+
+  public record Output(Set<ProviderInfo> providers) {}
+
+  @Inject
+  GetAiProviders(
+      DynamicSet<AiReviewProvider> aiReviewProviders,
+      Provider<CurrentUser> currentUser,
+      VersionedAiUserData.Factory tokenDataFactory,
+      PermissionBackend permissionBackend) {
+    this.aiReviewProviders = aiReviewProviders;
+    this.currentUser = currentUser;
+    this.tokenDataFactory = tokenDataFactory;
+    this.permissionBackend = permissionBackend;
+  }
+
+  @Override
+  public Response<Output> apply(AccountResource resource) throws Exception {
+    IdentifiedUser iu = resource.getUser();
+    Account.Id accountId = iu.getAccountId();
+
+    if (!iu.hasSameAccountId(currentUser.get())) {
+      permissionBackend.currentUser().check(GlobalPermission.ADMINISTRATE_SERVER);
+    }
+    VersionedAiUserData versionedAiUserData = tokenDataFactory.create(accountId).load();
+
+    Set<ProviderInfo> providersPlugins =
+        StreamSupport.stream(aiReviewProviders.entries().spliterator(), false)
+            .filter(ext -> versionedAiUserData.getToken(ext.getPluginName()).isPresent())
+            .map(this::getProviderInfo)
+            .collect(Collectors.toSet());
+    return Response.ok(new Output(providersPlugins));
+  }
+
+  private ProviderInfo getProviderInfo(Extension<AiReviewProvider> ext) {
+    AiReviewProvider aiProvider = ext.get();
+    return new ProviderInfo(
+        ext.getPluginName(), aiProvider.getDisplayName(), aiProvider.getModels());
+  }
+}
