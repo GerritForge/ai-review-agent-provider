092a98915c8b64ae4c170418cf44e7fc7291c734
Luca Milanesio <luca.milanesio@gmail.com>
2026-05-09 00:26:01 +0100

    Allow removing API keys from user profile
    Convert the GET of AiProviders to a proper REST-API collection and
    introduce the ability to remove an API key with an HTTP DELETE method.
    
    Change-Id: Id7bff3d4b11ef83c4fe0266ac7935aa0b59049ef

---
+++ src/main/java/com/gerritforge/gerrit/plugins/ai/provider/DeleteToken.java
@@ -0,0 +1,71 @@
+// Copyright (C) 2026 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.ai.provider;
+
+import com.gerritforge.gerrit.plugins.ai.provider.api.AiReviewProvider;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.extensions.common.Input;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.permissions.GlobalPermission;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+@Singleton
+class DeleteToken implements RestModifyView<AiProviderResource, Input> {
+  private final DynamicSet<AiReviewProvider> aiReviewProvidersSet;
+  private final Provider<CurrentUser> currentUser;
+  private final VersionedAiUserData.Factory tokenDataFactory;
+  private final PermissionBackend permissionBackend;
+
+  @Inject
+  DeleteToken(
+      DynamicSet<AiReviewProvider> aiReviewProvidersSet,
+      Provider<CurrentUser> currentUser,
+      VersionedAiUserData.Factory tokenDataFactory,
+      PermissionBackend permissionBackend) {
+    this.aiReviewProvidersSet = aiReviewProvidersSet;
+    this.currentUser = currentUser;
+    this.tokenDataFactory = tokenDataFactory;
+    this.permissionBackend = permissionBackend;
+  }
+
+  @Override
+  public Response<?> apply(AiProviderResource resource, Input input) throws Exception {
+    if (resource.getPlugin() == null || resource.getPlugin().isBlank()) {
+      throw new BadRequestException("Token provider plugin must be present and non-empty");
+    }
+    if (!aiReviewProvidersSet.plugins().contains(resource.getPlugin())) {
+      throw new BadRequestException(
+          "token refers to a non-existent or not-loaded ai-review-provider "
+              + resource.getPlugin());
+    }
+
+    IdentifiedUser iu = resource.getUser();
+    Account.Id accountId = iu.getAccountId();
+
+    if (!iu.hasSameAccountId(currentUser.get())) {
+      permissionBackend.currentUser().check(GlobalPermission.ADMINISTRATE_SERVER);
+    }
+
+    VersionedAiUserData tokenData = tokenDataFactory.create(accountId).load();
+    tokenData.removeToken(resource.getPlugin());
+
+    return Response.none();
+  }
+}
