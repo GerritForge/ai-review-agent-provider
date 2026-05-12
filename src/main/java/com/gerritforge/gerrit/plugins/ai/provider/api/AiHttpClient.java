797c56da35b931b0a2dd988429d0ce94f3303239
Luca Milanesio <luca.milanesio@gmail.com>
2026-05-12 22:16:38 +0100

    Add HTTP GET/POST shortcuts as default methods in AiHttpClient
    Most of the HTTP clients will have to send HTTP GET/POST with common
    parameters and headers:
    - application/json content-type
    - expecting 20x responses
    
    Introduce two new methods for sending HTTP GET/POST actions and add
    utility methods for generating the application/json content type
    headers.
    
    Change-Id: I3966485c32770d8a1f3ba4bf250df20c50787fbd

--- src/main/java/com/gerritforge/gerrit/plugins/ai/provider/api/AiHttpClient.java
+++ src/main/java/com/gerritforge/gerrit/plugins/ai/provider/api/AiHttpClient.java
@@ -11,14 +11,99 @@
 
 package com.gerritforge.gerrit.plugins.ai.provider.api;
 
+import com.google.common.net.MediaType;
 import java.io.Closeable;
 import java.io.IOException;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.message.BasicHeader;
 
 public interface AiHttpClient extends HttpClient, Closeable {
+
+  /** Create an HTTP Content-Type header */
+  default Header contentType(String contentType) {
+    return new BasicHeader(HttpHeaders.CONTENT_TYPE, contentType);
+  }
+
+  /** Create an HTTP Content-Type: application/json header */
+  default Header contentTypeApplicationJson() {
+    return contentType(MediaType.JSON_UTF_8.toString());
+  }
+
+  /** Create an HTTP Accept header */
+  default Header accept(String contentType) {
+    return new BasicHeader(HttpHeaders.ACCEPT, contentType);
+  }
+
+  /** Create an HTTP Accept application/json header */
+  default Header acceptApplicationJson() {
+    return accept(MediaType.JSON_UTF_8.toString());
+  }
+
+  /**
+   * Execute an HTTP POST request expecting a 20[0-2] status code, with a logic for extracting the
+   * error message from the response body.
+   *
+   * @param uri the URI of the AI service
+   * @param headers the additional HTTP headers for the POST request
+   * @param postBody the HTTP POST request body {@link HttpEntity}
+   * @param errorFromBody function for converting the response body in case of errors, `null` for no
+   *     error description
+   * @return the HTTP response if the status code is accepted as a success
+   * @throws IOException if any errors occur or if the status of the response does not satisfy the
+   *     accepted status function
+   */
+  default <T> T post(
+      String uri,
+      Header[] headers,
+      HttpEntity postBody,
+      ErrorBodyHandler errorFromBody,
+      ResponseBodyHandler<? extends T> responseBodyHandler)
+      throws IOException {
+    HttpPost httpPost = new HttpPost(uri);
+    httpPost.setHeaders(headers);
+    httpPost.setEntity(postBody);
+    return execute(
+        httpPost,
+        s -> s >= HttpStatus.SC_OK && s <= HttpStatus.SC_ACCEPTED,
+        errorFromBody,
+        responseBodyHandler);
+  }
+
+  /**
+   * Execute an HTTP GET request expecting a 20[0-2] status code, with a logic for extracting the
+   * error message from the response body.
+   *
+   * @param uri the URI of the AI service
+   * @param headers the additional HTTP headers for the GET request
+   * @param errorFromBody function for converting the response body in case of errors, `null` for no
+   *     error description
+   * @return the HTTP response if the status code is accepted as a success
+   * @throws IOException if any errors occur or if the status of the response does not satisfy the
+   *     accepted status function
+   */
+  default <T> T get(
+      String uri,
+      Header[] headers,
+      ErrorBodyHandler errorFromBody,
+      ResponseBodyHandler<? extends T> responseBodyHandler)
+      throws IOException {
+    HttpGet httpGet = new HttpGet(uri);
+    httpGet.setHeaders(headers);
+    return execute(
+        httpGet,
+        s -> s >= HttpStatus.SC_OK && s <= HttpStatus.SC_ACCEPTED,
+        errorFromBody,
+        responseBodyHandler);
+  }
 
   /**
    * Execute an HTTP request by checking the status code and logic for extracting the associated
