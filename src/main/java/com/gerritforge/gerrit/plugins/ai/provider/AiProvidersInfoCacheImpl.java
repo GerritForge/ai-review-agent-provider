50d8e99cd04f89f931979bf6282e786d56fc7c84
Luca Milanesio <luca.milanesio@gmail.com>
2026-05-14 18:23:41 +0100

    Cache list of AI Provider Models
    Remember the list of AI models in Gerrit persistent caches.
    
    The AI models are fetched for rendering in the AI chat user experience,
    however, their fetching and processing could have a long latency due to
    the inherent instability of AI systems and their associated throttling
    policy.
    
    The Gerrit UI should be stable and render regardless of the availability
    of the backend. Having the models cached and persisted helps preventing
    the UI freezing due to the AI backend instability.
    
    Change-Id: I272d5f4e10b7c2e144cb03737106fcb25e96c206

---
+++ src/main/java/com/gerritforge/gerrit/plugins/ai/provider/AiProvidersInfoCacheImpl.java
@@ -0,0 +1,68 @@
+// Copyright (C) 2026 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.ai.provider;
+
+import com.gerritforge.gerrit.plugins.ai.provider.api.AiReviewProvider;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.cache.Cache;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.server.cache.CacheModule;
+import com.google.inject.Inject;
+import com.google.inject.Scopes;
+import com.google.inject.TypeLiteral;
+import com.google.inject.name.Named;
+import java.time.Duration;
+import java.util.LinkedHashSet;
+
+class AiProvidersInfoCacheImpl implements AiProvidersInfoCache {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private static final Duration DEFAULT_CACHE_MAX_AGE = Duration.ofDays(1);
+  private static final int CACHE_VERSION = 1;
+
+  @VisibleForTesting public static final String AI_MODELS_BY_PLUGIN = "models";
+
+  private final Cache<String, LinkedHashSet<String>> modelsCache;
+
+  @Inject
+  AiProvidersInfoCacheImpl(
+      @Named(AI_MODELS_BY_PLUGIN) Cache<String, LinkedHashSet<String>> aiProvidersModels) {
+    this.modelsCache = aiProvidersModels;
+  }
+
+  @Override
+  public ProviderInfo getProviderInfo(
+      String pluginName, AiReviewProvider aiProvider, String apiKey) {
+    try {
+      return new AiProvidersInfoCache.ProviderInfo(
+          pluginName,
+          aiProvider.getDisplayName(),
+          modelsCache.get(pluginName, () -> new LinkedHashSet<>(aiProvider.getModels(apiKey))),
+          true);
+    } catch (Exception e) {
+      logger.atWarning().withCause(e).log(
+          "Unable to retrieve full AI provider info for plugin '%s' from cache", pluginName);
+      return AiProvidersInfoCache.emptyAiProviderInfo(pluginName, aiProvider.getDisplayName());
+    }
+  }
+
+  public static CacheModule getModule() {
+    return new CacheModule() {
+      @Override
+      protected void configure() {
+        persist(AI_MODELS_BY_PLUGIN, String.class, new TypeLiteral<LinkedHashSet<String>>() {})
+            .expireAfterWrite(DEFAULT_CACHE_MAX_AGE)
+            .version(CACHE_VERSION);
+        bind(AiProvidersInfoCache.class).to(AiProvidersInfoCacheImpl.class).in(Scopes.SINGLETON);
+      }
+    };
+  }
+}
