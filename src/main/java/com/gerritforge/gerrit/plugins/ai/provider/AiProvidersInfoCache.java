6fb05aa91769107eb868cf0774a548e1af14ec81
Luca Milanesio <luca.milanesio@gmail.com>
2026-05-14 18:23:41 +0100

    Cache list of AI Provider Models
    Remember the list of AI models in Gerrit persistent caches.
    
    The AI models are fetched for rendering in the AI chat user experience,
    however, their fetching and processing could have a long latency due to
    the inherent instability of AI systems and their associated throttling
    policy.
    
    The Gerrit UI should be stable and render regardless of the availability
    of the backend. Having the models cached and persisted helps preventing
    the UI freezing due to the AI backend instability.
    
    Change-Id: I272d5f4e10b7c2e144cb03737106fcb25e96c206

---
+++ src/main/java/com/gerritforge/gerrit/plugins/ai/provider/AiProvidersInfoCache.java
@@ -0,0 +1,28 @@
+// Copyright (C) 2026 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.ai.provider;
+
+import com.gerritforge.gerrit.plugins.ai.provider.api.AiReviewProvider;
+import java.io.Serializable;
+import java.util.Set;
+
+public interface AiProvidersInfoCache {
+
+  record ProviderInfo(String plugin, String displayName, Set<String> models, boolean enabled)
+      implements Serializable {}
+
+  static ProviderInfo emptyAiProviderInfo(String pluginName, String displayName) {
+    return new ProviderInfo(pluginName, displayName, Set.of(), false);
+  }
+
+  ProviderInfo getProviderInfo(String pluginName, AiReviewProvider aiReviewProvider, String apiKey);
+}
